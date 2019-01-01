612742eb613aa70284a4e8f07f2f6954b4eaf64b
root <vikas.pandey@ia.ooo>
2019-01-01 14:56:15 +0530

    Show old reviews below

--- wc-author-rating.php
+++ wc-author-rating.php
@@ -23,6 +23,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		global $jal_db_version;
 
 		$table_name = $wpdb->prefix . 'buyer_rating';
+		$table_name2 = $wpdb->prefix . 'user_selection';
 		
 		$charset_collate = $wpdb->get_charset_collate();
 
@@ -31,6 +32,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 			  `authorid` int(11) NOT NULL,
 			  `buyerid` int(11) NOT NULL,
 			  `rating_number` int(11) NOT NULL,
+			  `review_comment` varchar(255) NOT NULL,
 			  `total_points` int(11) NOT NULL,
 			  `created` datetime NOT NULL,
 			  `modified` datetime NOT NULL,
@@ -38,13 +40,92 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 			   PRIMARY KEY (`rating_id`)
 		) $charset_collate;";
 
+		$sql2 = "CREATE TABLE $table_name2 (
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `user_role` varchar(50) NOT NULL,
+			  `voter_role` varchar(100) NOT NULL,
+			   PRIMARY KEY (`id`)
+		) $charset_collate;";
+
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		dbDelta( $sql );
+		dbDelta( $sql2 );
 
 		add_option( 'jal_db_version', $jal_db_version );
 	}
 
 	register_activation_hook( __FILE__, 'jal_install' );
+
+
+	add_action('admin_menu', 'wc_author_rating');
+	function wc_author_rating(){
+	    add_menu_page('Author Rating', 'Author Rating', 'manage_options', 'get_all_users', 'get_all_users' );
+	   // add_submenu_page('get-all-users', 'Select User to Rate', 'Select User to Rate', 'manage_options', 'my-menu' );
+	}
+
+	function get_all_users(){
+		global $wpdb;
+		global $wb;
+		global $wp_roles;
+     	$roles = $wp_roles->get_names(); 
+     	$selectedUser = get_selected_user_role();
+     	if($selectedUser){
+     		$selectedVoter = explode(",", $selectedUser[0]->voter_role);
+     	}
+     	?>
+     	<h2>User Rating Settings</h2>
+     	<form id="user_roles" method="POST">     		
+			<table class="form-table">
+			    <tbody>
+					<tr>
+						<td><p>Select User To Rate</p></td>
+					</tr>
+					<tr>
+						<td>
+							<select name="user_role">
+								<?php foreach ( $wp_roles->roles as $key=>$value ): ?>
+								<option value="<?php echo $key; ?>"  <?php if(!empty($selectedUser[0]->user_role)) { echo ($selectedUser[0]->user_role == $key)? 'selected="selected"' : ''; } ?>><?php echo $value['name']; ?></option>
+								<?php endforeach; ?>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td><p>Select User Who can Rate(multiple can be selected)</p></td>
+					</tr>
+					<tr>
+						<td>
+							<select multiple="multiple" name="voter_role[]">
+								<?php foreach ( $wp_roles->roles as $key=>$value ): ?>
+								<?php $selected = in_array( $key, $selectedVoter ) ? ' selected="selected" ' : ''; echo $selected; ?>
+								<option value="<?php echo $key; ?>"  <?php if(!empty($selected)) { echo $selected; } ?>><?php echo $value['name']; ?></option>
+								<?php endforeach; ?>
+							</select>
+						</td>
+					</tr>				
+				</tbody>
+			</table>
+			<input class="button-primary" type="submit" name="Submit" value="Submit">
+		</form>
+<?php
+		$userRole = '';
+		if((!empty($_POST['user_role'])) && (isset($_POST)))  {
+			
+			$userRole = $_POST['user_role'];
+			$voterRole = implode(",", $_POST['voter_role']); 
+
+			if(empty($selectedUser[0]->user_role)){
+				$wpdb->insert( $wpdb->prefix.'user_selection', array('user_role' => $userRole,'voter_role' => $voterRole),array( '%s','%s'));
+			} else {
+				$wpdb->update($wpdb->prefix.'user_selection',array('user_role' => $userRole,'voter_role' => $voterRole),array( 'id' => 1 ),array('%s'),array( '%d' ));
+			}
+		}
+	}
+
+function get_selected_user_role(){
+		global $wpdb;
+		$query = "SELECT user_role , voter_role FROM  ".$wpdb->prefix."user_selection WHERE id = '1'";
+		return $wpdb->get_results($query);
+}
 
 	/* Register Buyer Ratings Menu in My account page */
 	/*
@@ -55,70 +136,128 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		$menu_links = array_slice( $menu_links, 0, 5, true ) 
 		+ array( 'buyer-ratings' => 'Buyer Ratings' )
 		+ array_slice( $menu_links, 5, NULL, true ); 
+	
+	/*Check If user is Allowed to vote*/
+		$cUser = wp_get_current_user();
+		$userCanRate = get_selected_user_role();
+		if($userCanRate) {
+		$CanVote = explode(",", $userCanRate[0]->voter_role);
+			if(!in_array($cUser->roles[0], $CanVote)) {
+	        	unset($menu_links['buyer-ratings']);
+			}
+		}
 		return $menu_links; 
 	}
 	/*
 	 * Step 2. Register Permalink Endpoint
 	 */
-	add_action( 'init', 'add_buyer_rating_endpoints' );
-	function add_buyer_rating_endpoints() {
-		add_rewrite_endpoint( 'buyer-ratings', EP_PAGES ); 
-	}
+add_action( 'init', 'add_buyer_rating_endpoints' );
+function add_buyer_rating_endpoints() {
+	add_rewrite_endpoint( 'buyer-ratings', EP_PAGES );
+}
 	/*
 	 * Step 3. Content for the new page in My Account, woocommerce_account_{ENDPOINT NAME}_endpoint
 	 */
-	add_action( 'woocommerce_account_buyer-ratings_endpoint', 'add_buyer_rating_menu_endpoints' );
-	function add_buyer_rating_menu_endpoints() {
-	 
-	 /*$args = array('role'=> 'shop_manager','order'=> 'DESC');*/
-	 $args = array('role'=> 'vendor_admin','order'=> 'DESC');
+add_action( 'woocommerce_account_buyer-ratings_endpoint', 'add_buyer_rating_menu_endpoints' );
+function add_buyer_rating_menu_endpoints() {
+	$cUrrentuser 			= 	wp_get_current_user();
+	$userToRate 			= 	get_selected_user_role();
+	if($userToRate ) :
+		$userCanAcessPage 	=  	explode(",", $userToRate[0]->voter_role);
+	endif;
+	
+	if((isset($userCanAcessPage)) && (!in_array($cUrrentuser->roles[0], $userCanAcessPage))) {
+		echo "<h3>cheating uhh !!!  ;) You are not Allowed to view this page.</h3>";
+	} else {
+		$userRoleToRate = '';
+		 (!empty($userToRate)) ? $userRoleToRate = $userToRate[0]->user_role : "shop_manager";
 
-		global $wpdb;
-		global $wb;
-		$buyers = get_users($args);	
+		 /*$args = array('role'=> 'shop_manager','order'=> 'DESC');*/
+		 $args = array('role'=> $userRoleToRate,'order'=> 'DESC');
 
-		if(empty($buyers)) {
-			echo "<h3>No User found!!</h3>";
-		} else {
-			echo '<a class="all_buyers" href=?bid=all>All Buyers</a>';	
-		}
+			global $wpdb;
+			global $wb;
+			$buyers = get_users($args);	
 
-		$buyer_IDS = array();
-		foreach ($buyers as $buyer) 
-		{
-	    	$buyer_IDS[] = $buyer->ID;
-		}
-		
-		if((!empty($_GET['bid'])) && (in_array($_GET['bid'],$buyer_IDS)))
-		{
-			$b_ID 				= $_GET['bid'];
-			$userdata 			= get_userdata($b_ID);
-			$user_nicename 		= (!empty($userdata)) ? $userdata->user_nicename : '';
-
-			$result 			= get_buyer_ratings($b_ID);
-
-			$average_rating 	= (!empty($result)) ? $result[0]->average_rating : '';
-			$rating_number  	= (!empty($result[0]->rating_number)) ? $result[0]->rating_number : '';
-
-			echo '<div class="buyer_star_rating_wrapper">';
-		   	echo '<span class="buyer_nickname">Rate '.$user_nicename.'</span>
-		   			<input name="rating" value="'.intval(floor($average_rating)).'" id="rating_star_'.$b_ID.'" type="hidden" postID="1" buyerID="'.$b_ID.'" ratingAVG="'.$average_rating.'" />';
-		   	if(empty($average_rating)){
-		   		echo '<span>No Ratings !!</span>';
-		   	} else {
-		   		echo'<div class="overall-rating">(Average Rating <span id="avgrat">'.$average_rating.'</span>
-	    		 Based on <span id="totalrat">'.$rating_number.'</span> rating)</span></div>';	    	
-		   	}
-		   	echo  '</div>';
-	    	
-		} else {
-			echo '<div class="buyers_list">';
-			foreach ($buyer_IDS as $key => $value) {
-				echo '<a class="buyer_link" href=?bid='.$value.'>Rate buyer_'.$value.'</a>';
+			if(empty($buyers)) {
+				echo "<h3>No User found!!</h3>";
+			} else {
+				echo '<a class="all_buyers" href=?bid=all>All Buyers</a>';	
 			}
-			echo '</div>';
+
+			$buyer_IDS = array();
+			foreach ($buyers as $buyer) 
+			{
+		    	$buyer_IDS[] = $buyer->ID;
+			}
+			
+			if((!empty($_GET['bid'])) && (in_array($_GET['bid'],$buyer_IDS)))
+			{
+				$b_ID 						= $_GET['bid'];
+				$userdata 					= get_userdata($b_ID);
+				$user_nicename 				= (!empty($userdata)) ? $userdata->user_nicename : '';
+
+				$result 					= get_buyer_ratings($b_ID);
+				$result_review_comment 		= get_buyer_review_comment($b_ID);
+				$review_comment 			= (!empty($result_review_comment[0]->review_comment)) ? $result_review_comment[0]->review_comment : '';
+				$average_rating 			= (!empty($result)) ? $result[0]->average_rating : '';
+				$rating_number  			= (!empty($result[0]->rating_number)) ? $result[0]->rating_number : '';
+
+				echo '<form id="author_review_submit" method="POST"><div class="buyer_star_rating_wrapper">';
+			   	echo '<span class="buyer_nickname">Rate '.$user_nicename.'</span>
+			   			<input name="rating" value="'.intval(floor($average_rating)).'" id="rating_star_'.$b_ID.'" type="hidden" postID="1" buyerID="'.$b_ID.'" ratingAVG="'.$average_rating.'" />';
+			   	if(empty($average_rating)){
+			   		echo '<span>No Ratings !!</span>';
+			   	} else {
+			   		echo'<div class="overall-rating">(Average Rating <span id="avgrat">'.$average_rating.'</span>
+		    		 Based on <span id="totalrat">'.$rating_number.'</span> rating)</span></div>';	    	
+			   	}
+			   	echo '<div class="review_comment">
+			   			<br/>
+			   			<label>Comment Your Review</label>
+			   			<textarea name="review_comment" id="review_comment">'.$review_comment.'</textarea>
+			   		  </div>';
+			   	echo '<br/><input type="submit" name="Submit" value="Submit Review" id="Submit">';
+			   	echo  '</div>';
+
+			   	$buyer_all_rating 					= buyer_all_ratings($b_ID);
+			   	if(!empty($buyer_all_rating)) {
+				   	echo '<div class="all_buyer_review"><h3>Reviews</h3>';
+				   	foreach ($buyer_all_rating as $bRating) {
+				   		$bRating_number 			= $bRating->total_points;
+				   		$ratingAuthorId 			= $bRating->authorid;
+
+				   		$authorUserdata 			= get_userdata($ratingAuthorId);
+						$ratingAuthor 				= (!empty($authorUserdata)) ? $authorUserdata->user_nicename : '';
+
+
+				   		echo '<div class="buyer_star_rating"><div class="buyer_name">'.$ratingAuthor.'</div>
+				   			<div class="buyer_rating_comment_wrapper"><ul class="buyer_rating_ul">';
+				   		for($i=0;$i<=4;$i++)
+				   		{
+				   			if(($bRating_number <= $i)){
+				   				echo '<span class="dashicons dashicons-star-empty"></span>';
+				   			} else {
+				   				echo '<span class="dashicons dashicons-star-filled"></span>';
+				   			}			   					
+				   		}
+				   		echo '</ul>
+				   			  <div class="buyer_review_comment">'.$bRating->review_comment.'</div></div>';
+				   	}
+				   	echo '</div>';
+				}
+		    	
+			} else {
+				echo '<div class="buyers_list">';
+				foreach ($buyer_IDS as $key => $value) {
+					echo '<a class="buyer_link" href=?bid='.$value.'>Rate buyer_'.$value.'</a>';
+				}
+				echo '</div></form>';
+			}
 		}
-	}
+}
+
+add_shortcode( 'author-rating', 'add_buyer_rating_menu_endpoints' );
 
 	/*Inclue CSS and JS file*/
 
@@ -142,15 +281,23 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		return $wpdb->get_results($query);
 	}
 
+	function get_buyer_review_comment($bid)
+	{
+		$Ccuthorid = current_author();
+		global $wpdb;
+		$query = "SELECT review_comment FROM  ".$wpdb->prefix."buyer_rating WHERE buyerid = $bid AND authorid = $Ccuthorid";
+		return $wpdb->get_results($query);
+	}
+
 	add_action('wp_ajax_rate_buyer', 'rate_buyer');
 	add_action('wp_ajax_nopriv_rate_buyer', 'rate_buyer');
 
 	function rate_buyer() {
 
 		if(!empty($_POST['points'])){
-		    $authorid = '1'; //$_POST['authorid'];
-		    //$buyerid = '4';
+		    $authorid = current_author();
 		    $buyerid = $_POST['bid'];
+		    $rvw_cmt = $_POST['review_comment'];
 		    $rating_default_number = 1;
 		    $points = $_POST['points'];
 			global $wpdb;
@@ -164,11 +311,10 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		        $points = $prevRatingResult['total_points'] + $points;
 
 			    //Update rating data into the database       
-			    $wpdb->update( $wpdb->prefix.'buyer_rating', array('rating_number' => $rating_default_number,'total_points' => $points,'created' => date("Y-m-d H:i:s"),'modified' => date("Y-m-d H:i:s"),),array( 'authorid' => $authorid, 'buyerid' => $buyerid),array( '%s', '%d','%s','%s','%s'),array( '%d','%d'));
-
+			    $wpdb->update( $wpdb->prefix.'buyer_rating', array('rating_number' => $rating_default_number,'total_points' => $points,'review_comment'=>$rvw_cmt,'created' => date("Y-m-d H:i:s"),'modified' => date("Y-m-d H:i:s"),),array( 'authorid' => $authorid, 'buyerid' => $buyerid),array( '%s', '%d','%s','%s','%s'),array( '%d','%d'));
 		    else:
 		        //Insert rating data into the database
-		        $wpdb->insert( $wpdb->prefix.'buyer_rating', array('authorid' => $authorid,'buyerid' => $buyerid, 'rating_number' => $rating_default_number,'total_points' => $points,'created' => date("Y-m-d H:i:s"),'modified' => date("Y-m-d H:i:s"),),array( '%s', '%d','%s','%s','%s'));
+		        $wpdb->insert( $wpdb->prefix.'buyer_rating', array('authorid' => $authorid,'buyerid' => $buyerid,'review_comment'=>$rvw_cmt, 'rating_number' => $rating_default_number,'total_points' => $points,'created' => date("Y-m-d H:i:s"),'modified' => date("Y-m-d H:i:s"),),array( '%s', '%d','%s','%s','%s'));	        
 		    endif;
 		    
 		    //Fetch rating deatails from database
@@ -179,11 +325,24 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		        $ratingRow['status'] = 'ok';
 		    }else{
 		        $ratingRow['status'] = 'err';
-		    }  
+		    }
 		    echo $ratingRow['status'];
 		    die();
 		}
 	}
+
+	function current_author(){
+		$CurrAuthor = wp_get_current_user();
+		return $CurrAuthor->ID;
+	}
+
+	function buyer_all_ratings($bid){
+		global $wpdb;
+		$all_rating_query = "SELECT total_points,review_comment , authorid FROM ". $wpdb->prefix."buyer_rating WHERE buyerid=".$bid;
+		return $wpdb->get_results($all_rating_query);
+	}
+
+
  /*check if woocommerce plugin is active*/
 }
 ?>
